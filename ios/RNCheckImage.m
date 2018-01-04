da9ad01a08dd561ec9b774698662171bad0321ac
zhangguozhong <494316382@qq.com>
2018-01-04 22:06:49 +0800

    提交测试逻辑

--- ios/RNCheckImage.m
+++ ios/RNCheckImage.m
@@ -9,5 +9,23 @@
 }
 RCT_EXPORT_MODULE()
 
+RCT_EXPORT_METHOD(checkCemera:(NSString*)name resovler:(RCTPromiseResolveBlock)resovler rejecter:(RCTPromiseRejectBlock)reject){
+    if (name&&name.length>0) {
+        resovler(name);
+    }else{
+        reject(@"code",@"message",nil);
+    }
+}
+
+RCT_EXPORT_METHOD(check:(NSString*)name callback:(RCTResponseSenderBlock)callback){
+    NSArray *callbackArr = @[@"zhang",@"wang",@"tang"];
+    if (name) {
+        callback(@[[NSNull null],callbackArr]);
+    }else{
+        callback(@[[NSNull null],name]);
+    }
+}
+
+
 @end
   
