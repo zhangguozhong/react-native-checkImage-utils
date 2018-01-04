ef91e4be50d970b1dec0db6020fea5fa53b9542c
zhangguozhong <494316382@qq.com>
2018-01-04 19:42:43 +0800

    初次提交

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNCheckImage } = NativeModules;
+
+export default RNCheckImage;
---
+++ ios/RNCheckImage.m
@@ -0,0 +1,13 @@
+
+#import "RNCheckImage.h"
+
+@implementation RNCheckImage
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
