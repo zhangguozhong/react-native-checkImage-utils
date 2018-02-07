151f6fbf57a65200d14349d689941fef3d6a993f
zhangguozhong <494316382@qq.com>
2018-02-07 21:31:28 +0800

    增加TestObject文件

---
+++ ios/NewModule/TestObject.h
@@ -0,0 +1,15 @@
+//
+//  TestObject.h
+//  RNCheckImage
+//
+//  Created by 张国忠 on 2018/2/7.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestObject : NSObject
+
+- (void)testFuc;
+
+@end
---
+++ ios/NewModule/TestObject.m
@@ -0,0 +1,17 @@
+//
+//  TestObject.m
+//  RNCheckImage
+//
+//  Created by 张国忠 on 2018/2/7.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
+- (void)testFuc {
+    NSLog(@"输出很牛逼的函数");
+}
+
+@end
